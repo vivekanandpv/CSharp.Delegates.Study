3c46d7a4dbaa58f4951ef44b0d4a5f47d900fd08
vivek <vivek@athenaeum.in>
2021-06-01 15:33:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Delegates.Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
