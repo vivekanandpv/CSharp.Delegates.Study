71332865b8f92e269847d1d815eda65dfa0e9f14
vivek <vivek@athenaeum.in>
2021-06-01 17:44:30 +0530

    done

--- Program.cs
+++ Program.cs
@@ -2,45 +2,25 @@
 
 namespace CSharp.Delegates.Study
 {
-    //  delegate type is derived from MulticastDelegate
-    //  https://docs.microsoft.com/en-us/dotnet/api/system.multicastdelegate?view=net-5.0
-    //  This means, delegates inherently support multiple subscribers
-
-    public delegate void PrintMessage(string message);  
+    public delegate void PrintMessage(string message);
     class Program
     {
         static void Main(string[] args)
         {
-            PrintMessage delegateInstance1 = Foo;
+            //  this is anonymous function implementation
+            //  Lambda expressions emerge from this point
+            //  note the delegate keyword use in other context
+            PrintMessage anonymousDelegate = delegate (string str)
+            {
+                Console.WriteLine($"Anonymous function delegate executing: {str}");
+            };
 
-            var sample = new Sample();
-
-            //  creating an instance of the delegate with an instance method
-            delegateInstance1 +=  sample.Baz;   //  multicasting
-            delegateInstance1 +=  sample.Baz;   //  duplicate subscription is allowed
-            delegateInstance1 +=  sample.Baz;
-            delegateInstance1 -=  sample.Baz;   //  unsubscribe
-
-            Bar(delegateInstance1, "Good Afternoon");
-        }
-
-        static void Foo(string m)
-        {
-            Console.WriteLine($"Static Implementation: {m}");
+            Bar(anonymousDelegate, "Good Afternoon");
         }
 
         static void Bar(PrintMessage pmDelegate, string message)
         {
-            //  pmDelegate.Invoke(message); //  old way
-            pmDelegate(message); //  modern approach
-        }
-    }
-
-    class Sample
-    {
-        public void Baz(string m)
-        {
-            Console.WriteLine($"Instance Implementation: {m}");
+            pmDelegate(message);
         }
     }
 }
